6f2b46b86fcd7577b938f92d9d0ae4f6e1f2ca48
claude156 <89487461+claude156@users.noreply.github.com>
2024-10-23 23:33:25 -0500

    Create script.js

---
+++ chapter2project/project2.js
@@ -0,0 +1,29 @@
+// Richard Nahano
+// date 10/23/24
+
+myName = "Richard Nahano";
+para1 = document.getElementById("p1");
+para1.textContent = myName;
+
+num1 = 5;
+num2 = 10;
+num3 = 3;
+num4 = 6;
+
+numberSum = num1 + num2;
+document.getElementById("p2").textContent = numberSum;
+
+numberMult = num3 * num4;
+document.getElementById("p3").textContent = numberMult;
+
+myNameAddNum = +num4;
+
+document.getElementById("p4").textContent = myNameAddNum;
+
+myNameMultNum = 5 * num1;
+document.getElementById("p5").textContent = myNameMultNum;
+
+ageCompare = 30;
+
+// num1 * num2;
+document.getElementById("p6").textContent = ageCompare;
