0e2e56b4e9d6bb6a2ea0d2b65187998c694b21d7
claude156 <89487461+claude156@users.noreply.github.com>
2024-11-07 00:52:31 -0600

    Create script.js

---
+++ Chapter3project/script.js
@@ -0,0 +1,22 @@
+//My name is Richard Nahano
+//Today's Date 11/06/24
+// Richard Nahano
+// 11/06/24
+
+const fullName = "Richard Nahano";
+const DesireSalary = 125000;
+const VeteranStatus = false;
+const Array = ["billy", "Augustin", "Basabose"];
+const DresireSalaryFriends = [70000, 100000, 120000];
+
+const Object = {
+  name: "richartd",
+  lastName: "Nahano",
+  AnnualSalary: 100000,
+};
+console.log(fullName);
+console.log(DesireSalary);
+console.log(VeteranStatus);
+console.log(Array);
+console.log(DresireSalaryFriends);
+console.log(Object);
